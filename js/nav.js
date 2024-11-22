0af644d8d9d162fd218096a88b717be6f4fa8bb7
Ivaylo Chernev <ivko0527@gmail.com>
2024-11-22 23:56:22 +0200

    a
    a

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://ivakalol.github.io/AboutMe/includes/nav.html')
+    fetch('/includes/nav.html')
         .then(response => response.text())
         .then(data => { 
             document.getElementById('nav-placeholder').innerHTML = data;
